9d50e97cf19f51331626ca8618de7695615580e5
Malliaridis Konstantinos <terminal_gr@yahoo.com>
2025-12-08 20:13:25 +0200

    hello world

---
+++ hello.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * PHP Hello World Script
+ * This simple script outputs the text "Hello, World!" to the browser.
+ * It is typically the first program written when learning a new language.
+ */
+
+// Use the echo construct to output the string to the standard output.
+// In a web server context, this output is sent as the HTML response body.
+echo "<h1>Hello world from backend!!!</h1>";
+
+?>
